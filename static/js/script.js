49e05a9022a4c70c408115b7cf9009509ff8b1de
FITSTARGILLY2643 <fitstargilii1194@gmail.com>
2022-09-25 20:14:28 +0300

    Created the what we do and testimonial section

---
+++ static/js/script.js
@@ -0,0 +1,14 @@
+var i = 0;
+var txt = 'Lorem ipsum dummy text blabla.';
+var speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("capacity").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+
+
