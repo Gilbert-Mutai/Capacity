0a358949b55fb0d9a373a61b13040c145eee617f
FITSTARGILLY2643 <fitstargilii1194@gmail.com>
2022-10-01 21:24:43 +0300

    Finalize stylings

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,15 @@
 var i = 0;
-var txt = 'Lorem ipsum dummy text blabla.';
-var speed = 50;
+var txt = 'We are Capacity Limited. IoT & AI Technology-based, Smart Utility Metering & Utility Auditing Company in water, gas and energy.';
+var time = 30;
 
 function typeWriter() {
   if (i < txt.length) {
-    document.getElementById("capacity").innerHTML += txt.charAt(i);
+    document.getElementById("welcome-message").innerHTML += txt.charAt(i);
+    if(i==23){
+      document.getElementById("welcome-message").innerHTML +="<br>"
+      }
     i++;
-    setTimeout(typeWriter, speed);
+    setTimeout(typeWriter, time);
   }
 }
 
